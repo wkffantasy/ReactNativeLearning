a9470f0a42843831a4ce81812f9849e01b44520e
fantasy <1239002985@qq.com>
2017-01-05 14:53:59 +0800

    add some lib

--- android/app/src/main/java/com/reactnativelearning/MainApplication.java
+++ android/app/src/main/java/com/reactnativelearning/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
+import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.horcrux.svg.RNSvgPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
+            new ReactNativeAudioPackage(),
             new RNSvgPackage()
       );
     }
